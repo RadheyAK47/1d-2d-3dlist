f37dbd684254479da735f120d6f558892d21b99a
RadheyAK47 <76564704+RadheyAK47@users.noreply.github.com>
2023-04-01 20:44:53 +0530

    Add files via upload

---
+++ 1d,2d,3dlist.py
@@ -0,0 +1,15 @@
+#1d list
+l=["red","blue","yellow","green","white"]
+print(l)
+print(l[2])
+
+#2d list
+r=[["red",5,2],["blue",2,1],["yellow",6,3],["green",8,7],["white",10,0]]
+print(r)
+print(r[2][0])
+print(r[4][1])
+print(r[1][2])
+
+#3d list
+a=[[["red",5,2],["blue",2,1],["yellow",6,3],["green",8,7],["white",10,0]]]
+print(a[0][0][0])
